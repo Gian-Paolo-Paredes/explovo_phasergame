ed40d9f4892799f0d157fb10a378f881d6a937dd
Alex Smirnov <asmirno1@ucsc.edu>
2017-05-08 18:37:15 -0700

    Added particle prototype files.

--- js/Main.js
+++ js/Main.js
@@ -1,3 +1,23 @@
+//logging functionality
+function l(itemToLog){
+   if(typeof(itemToLog)==Object){
+      console.log(JSON.stringify(itemToLog));
+   }else{
+      console.log(itemToLog);
+   }
+}
+
+function transformOverAngle(angle, x, y){
+   //angle given in radians
+   cosine = Math.cos(angle);
+   sine = Math.sin(angle);
+   xprime = (cosine*x)+(-1*(sine*y));
+   yprime = (sine*x)+(cosine*y);
+   var returnObj = {x: xprime, y: yprime};
+   return returnObj;
+}
+
+var GameTime;
 var GameContainer = { };
 
 //---/Game States
@@ -50,9 +70,74 @@ GameContainer.stGame.prototype = {
 
    },//end_preload
    create: function() {
+      player = this.add.sprite(game.world.centerX, game.world.centerY, "Player");
+      player.anchor.set(0.5);
+
+      game.physics.arcade.enable(player);
+      player.enableBody = true;
+      player.collideWorldBounds = true;
+
+      player.rotation = game.physics.arcade.angleToPointer(player);
+
+      emitter = game.add.emitter(0, 0, 1000, true, false); //positionx, positiony, numberparticles, collide arcade, collide world
+      this.world.moveDown(emitter);
+
+
+      emitter.makeParticles('Particle');
+
+      emitter.forEach(function(particle) {
+         particle.body.allowGravity = false;
+      }, this);
+
+      var offset = Math.sqrt(Math.pow(game.input.mousePointer.x-emitter.x, 2)+Math.pow(game.input.mousePointer.y-emitter.y, 2));
+      l(offset);
+
+      emitter.lifespan = 1000;
+
+
+      emitter.maxParticleSpeed = new Phaser.Point(game.input.mousePointer.x-emitter.x+offset, game.input.mousePointer.y-emitter.y+offset);
+      emitter.minParticleSpeed = new Phaser.Point(game.input.mousePointer.x-emitter.x-offset, game.input.mousePointer.y-emitter.y-offset);
+
+      //emitter.minParticleSpeed = new Phaser.Point(200,-50);
 
    },//end_create
    update: function(){
+      player.rotation = game.physics.arcade.angleToPointer(player);
+      //l("rotation:" + player.rotation);
+
+
+
+
+
+
+   if (game.input.activePointer.isDown){
+
+      offsetMax = 200;
+      offsetMin = 80;
+      offsetNarrowing = 0.5;
+      offsetNoise = 10;
+      emitterSpriteOffsetX = 25;
+      emitterSpriteOffsetY = 15;
+
+      emitter.y = player.y + transformOverAngle(player.rotation, emitterSpriteOffsetX, emitterSpriteOffsetY).y;
+      emitter.x = player.x + transformOverAngle(player.rotation, emitterSpriteOffsetX, emitterSpriteOffsetY).x;
+
+      var offset = Math.sqrt(Math.pow(game.input.mousePointer.x-emitter.x, 2)+Math.pow(game.input.mousePointer.y-emitter.y, 2));
+      if (offset > offsetMax){
+         offset = offsetMax;
+      }else if(offset < offsetMin){
+         offset = offsetMin;
+      }
+      offset = offsetNarrowing*(offsetMax-offset) + offsetNoise;
+      l(offset);
+
+      emitter.lifespan = 800 + (1/offsetNarrowing)*offset;
+
+      emitter.maxParticleSpeed = new Phaser.Point(game.input.mousePointer.x-emitter.x+offset, game.input.mousePointer.y-emitter.y+offset);
+      emitter.minParticleSpeed = new Phaser.Point(game.input.mousePointer.x-emitter.x-offset, game.input.mousePointer.y-emitter.y-offset);
+
+      emitter.emitParticle();
+   }
 
    },//end_update
    render: function() {
