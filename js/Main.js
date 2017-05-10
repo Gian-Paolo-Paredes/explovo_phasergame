9c0f6c8b0f38835d18c2e8abd66aaa1b9d84c82b
Alex Smirnov <asmirno1@ucsc.edu>
2017-05-10 01:16:25 -0700

    Cleaned up code in Main.js for the particle prototype, limited particle range to radius around the player, also added commentary. Added Distance Formula to Utility.js

--- js/Main.js
+++ js/Main.js
@@ -1,23 +1,3 @@
-//logging functionality
-function l(itemToLog){
-   if(typeof(itemToLog)==Object){
-      console.log(JSON.stringify(itemToLog));
-   }else{
-      console.log(itemToLog);
-   }
-}
-
-function transformOverAngle(angle, x, y){
-   //angle given in radians
-   cosine = Math.cos(angle);
-   sine = Math.sin(angle);
-   xprime = (cosine*x)+(-1*(sine*y));
-   yprime = (sine*x)+(cosine*y);
-   var returnObj = {x: xprime, y: yprime};
-   return returnObj;
-}
-
-var GameTime;
 var GameContainer = { };
 
 //---/Game States
@@ -47,10 +27,10 @@ GameContainer.stPreloadAssets.prototype = {
 
       //---/ load all assets
       //load non-atlas assets
-      this.load.images(["Player", "Particle"], ["Firefighter.png", "WFParticle.png"]);
+      this.load.images(["Player", "Particle", "Test_Building1", "Test_Building2"], ["Firefighter.png", "WFParticle.png", "proto_Build1.png", "proto_Build2.png"]);
 
-      //load the atlas
-      this.load.atlas("atlas", "atlas.png", "atlas.json", Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+      //load assets from atlas (if applicable)
+
 
    },//end_preload
    create: function(){
@@ -62,14 +42,47 @@ GameContainer.stPreloadAssets.prototype = {
 //Main Game State
 GameContainer.stGame = function() {
 
+      var waterStreamMaxDistance;
+      var particleVelocityOffsetMax;
+      var particleVelocityOffsetMin;
+      var particleVelocityOffsetNarrowing;
+      var particleVelocityOffsetNoise;
+      var emitterSpriteOffsetX;
+      var emitterSpriteOffsetY;
+
+      var particleVelocityOffset;
+      var distMouseCursorToEmitter;
+      var waterParticleLifetimeConstant;
+
 };
 GameContainer.stGame.prototype = {
    preload: function(){
       game.time.advancedTiming = true;
 
-
    },//end_preload
    create: function() {
+
+   //--/ variable assignments
+      //--/ water particle emitter variables
+         //Max distance the stream of water reaches
+         waterStreamMaxDistance = 250; //250 seems optimal for a 600 by 800 screen
+         //particleVelocityOffset is the distance of the player to the mouse cursor; Max and min limit that distance
+         particleVelocityOffsetMax = 200;
+         particleVelocityOffsetMin = 80;
+         //narrowing
+         particleVelocityOffsetNarrowing = 0.5;
+         // adds left-right variation in the longest possible stream of particles to resemble a jet of water
+         particleVelocityOffsetNoise = 10;
+         //the location in pixels from the center of the sprite (0, 0) to the location of the emitter
+         //    used to calculate new positions for the emitter as player rotates
+         emitterSpriteOffsetX = 25;
+         emitterSpriteOffsetY = 15;
+         // a "game feel" variable that both affects distance and where the cursor should be
+         //    positioned to achieve a certain distance with the particles. Phaser does not seem
+         //    to have particle limiting by distance so keep this value at 4 for now.
+         waterParticleLifetimeConstant = 4;
+
+      //test sprite functionality, player added here
       player = this.add.sprite(game.world.centerX, game.world.centerY, "Player");
       player.anchor.set(0.5);
 
@@ -79,9 +92,10 @@ GameContainer.stGame.prototype = {
 
       player.rotation = game.physics.arcade.angleToPointer(player);
 
-      emitter = game.add.emitter(0, 0, 1000, true, false); //positionx, positiony, numberparticles, collide arcade, collide world
-      this.world.moveDown(emitter);
 
+      //--/ creating the water particle emitter
+      emitter = game.add.emitter(0, 0, 1200, true, false); //positionx, positiony, numberparticles, collide arcade, collide world
+      this.world.moveDown(emitter);
 
       emitter.makeParticles('Particle');
 
@@ -89,52 +103,41 @@ GameContainer.stGame.prototype = {
          particle.body.allowGravity = false;
       }, this);
 
-      var offset = Math.sqrt(Math.pow(game.input.mousePointer.x-emitter.x, 2)+Math.pow(game.input.mousePointer.y-emitter.y, 2));
-      l(offset);
-
-      emitter.lifespan = 1000;
-
-
-      emitter.maxParticleSpeed = new Phaser.Point(game.input.mousePointer.x-emitter.x+offset, game.input.mousePointer.y-emitter.y+offset);
-      emitter.minParticleSpeed = new Phaser.Point(game.input.mousePointer.x-emitter.x-offset, game.input.mousePointer.y-emitter.y-offset);
-
-      //emitter.minParticleSpeed = new Phaser.Point(200,-50);
-
    },//end_create
    update: function(){
       player.rotation = game.physics.arcade.angleToPointer(player);
-      //l("rotation:" + player.rotation);
-
-
-
-
-
 
    if (game.input.activePointer.isDown){
-
-      offsetMax = 200;
-      offsetMin = 80;
-      offsetNarrowing = 0.5;
-      offsetNoise = 10;
-      emitterSpriteOffsetX = 25;
-      emitterSpriteOffsetY = 15;
 
       emitter.y = player.y + transformOverAngle(player.rotation, emitterSpriteOffsetX, emitterSpriteOffsetY).y;
       emitter.x = player.x + transformOverAngle(player.rotation, emitterSpriteOffsetX, emitterSpriteOffsetY).x;
 
-      var offset = Math.sqrt(Math.pow(game.input.mousePointer.x-emitter.x, 2)+Math.pow(game.input.mousePointer.y-emitter.y, 2));
-      if (offset > offsetMax){
-         offset = offsetMax;
-      }else if(offset < offsetMin){
-         offset = offsetMin;
+      distMouseCursorToEmitter = distanceBetween(game.input.mousePointer.x, game.input.mousePointer.y, emitter.x, emitter.y);
+
+      // handles the "spread" of water particles (the further you aim the narrower the stream)
+      particleVelocityOffset = distMouseCursorToEmitter ;
+      if (particleVelocityOffset > particleVelocityOffsetMax){
+         particleVelocityOffset = particleVelocityOffsetMax;
+      }else if(particleVelocityOffset < particleVelocityOffsetMin){
+         particleVelocityOffset = particleVelocityOffsetMin;
       }
-      offset = offsetNarrowing*(offsetMax-offset) + offsetNoise;
-      l(offset);
+      particleVelocityOffset = particleVelocityOffsetNarrowing*(particleVelocityOffsetMax-particleVelocityOffset) + particleVelocityOffsetNoise;
 
-      emitter.lifespan = 800 + (1/offsetNarrowing)*offset;
+      //l("particleVelocityOffset: " + particleVelocityOffset);
 
-      emitter.maxParticleSpeed = new Phaser.Point(game.input.mousePointer.x-emitter.x+offset, game.input.mousePointer.y-emitter.y+offset);
-      emitter.minParticleSpeed = new Phaser.Point(game.input.mousePointer.x-emitter.x-offset, game.input.mousePointer.y-emitter.y-offset);
+      emitter.lifespan = waterStreamMaxDistance*waterParticleLifetimeConstant;
+
+      if(distMouseCursorToEmitter > waterStreamMaxDistance){
+         var similarTriangleProportion = waterStreamMaxDistance/distMouseCursorToEmitter;
+         emitterToMouseDistanceX = similarTriangleProportion*(game.input.mousePointer.x-emitter.x);
+         emitterToMouseDistanceY = similarTriangleProportion*(game.input.mousePointer.y-emitter.y);
+      }else{
+         emitterToMouseDistanceX = game.input.mousePointer.x-emitter.x;
+         emitterToMouseDistanceY = game.input.mousePointer.y-emitter.y;
+      }
+
+      emitter.maxParticleSpeed = new Phaser.Point(emitterToMouseDistanceX+particleVelocityOffset, emitterToMouseDistanceY+particleVelocityOffset);
+      emitter.minParticleSpeed = new Phaser.Point(emitterToMouseDistanceX-particleVelocityOffset,emitterToMouseDistanceY-particleVelocityOffset);
 
       emitter.emitParticle();
    }
