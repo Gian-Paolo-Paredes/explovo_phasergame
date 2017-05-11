6834065d884fc025a4120e2a053aab314c84b801
Alex Smirnov <asmirno1@ucsc.edu>
2017-05-10 22:04:03 -0700

    Preparing feature-water_particles for merge with state structure currently in develop.

--- js/states/stGame.js
+++ js/states/stGame.js
@@ -1,47 +1,5 @@
-var GameContainer = { };
-
-//---/Game States
-// Boot State
-GameContainer.stBoot = function() {};
-GameContainer.stBoot.prototype = {
-   init: function(){
-      l("boot_init");
-      this.stage.disableVisibilityChange = true;
-   },//end_init
-   preload: function(){
-      l("boot_preload");
-      this.load.path="assets/img/"; //sets initial load path to images folder
-   },//end_preload
-   create: function(){
-      this.state.start("stPreloadAssets");
-   }//end_create
-};
-
-// Asset Preload State
-GameContainer.stPreloadAssets = function() {
-
-};
-GameContainer.stPreloadAssets.prototype = {
-   preload: function(){
-      l("PreloadAssets_preload");
-
-      //---/ load all assets
-      //load non-atlas assets
-      this.load.images(["Player", "Particle", "Test_Building1", "Test_Building2"], ["Firefighter.png", "WFParticle.png", "proto_Build1.png", "proto_Build2.png"]);
-
-      //load assets from atlas (if applicable)
-
-
-   },//end_preload
-   create: function(){
-      l("PreloadAssets_create");
-      this.state.start("stGame");
-   },//end_create
-};
-
-//Main Game State
-GameContainer.stGame = function() {
-
+//Main this.game State
+var stGame = function(game) {
       var waterStreamMaxDistance;
       var particleVelocityOffsetMax;
       var particleVelocityOffsetMin;
@@ -54,18 +12,15 @@ GameContainer.stGame = function() {
       var distMouseCursorToEmitter;
       var waterParticleLifetimeConstant;
 
-
-      var particleBuildingOnCollision;
-
 };
-GameContainer.stGame.prototype = {
+stGame.prototype = {
    preload: function(){
-      game.time.advancedTiming = true;
+      this.game.time.advancedTiming = true;
 
    },//end_preload
    create: function() {
 
-      game.physics.startSystem(Phaser.Physics.ARCADE);
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
    //--/ variable assignments
       //--/ water particle emitter variables
@@ -82,25 +37,25 @@ GameContainer.stGame.prototype = {
          //    used to calculate new positions for the emitter as player rotates
          emitterSpriteOffsetX = 25;
          emitterSpriteOffsetY = 15;
-         // a "game feel" variable that both affects distance and where the cursor should be
+         // a "this.game feel" variable that both affects distance and where the cursor should be
          //    positioned to achieve a certain distance with the particles. Phaser does not seem
          //    to have particle limiting by distance so keep this value at 4 for now.
          waterParticleLifetimeConstant = 4;
 
       //test sprite functionality, player added here
-      player = this.add.sprite(game.world.centerX, game.world.centerY, "Player");
+      player = this.add.sprite(this.game.world.centerX, this.game.world.centerY, "Player");
       player.anchor.set(0.5);
 
-      game.physics.arcade.enable(player);
+      this.game.physics.arcade.enable(player);
       player.enableBody = true;
       player.collideWorldBounds = true;
 
-      player.rotation = game.physics.arcade.angleToPointer(player);
+      player.rotation = this.game.physics.arcade.angleToPointer(player);
 
-      cursors = game.input.keyboard.createCursorKeys();
+      cursors = this.game.input.keyboard.createCursorKeys();
 
       //--/ creating the water particle emitter
-      emitter = game.add.emitter(game, 0, 0);
+      emitter = this.game.add.emitter(this.game, 0, 0);
 
       this.world.moveDown(emitter);
 
@@ -113,15 +68,15 @@ GameContainer.stGame.prototype = {
       }, this);
 
       // test particle collision functionality, building added here
-       building = this.add.sprite(game.world.centerX/2, game.world.centerY, "Test_Building1");
+       building = this.add.sprite(this.game.world.centerX/2, this.game.world.centerY, "Test_Building1");
        building.anchor.set(0.5);
-       game.physics.arcade.enable(building);
+       this.game.physics.arcade.enable(building);
        building.enableBody = true;
        building.body.immovable = true;
 
-       building2 = this.add.sprite(game.world.centerX*1.5, game.world.centerY, "Test_Building1");
+       building2 = this.add.sprite(this.game.world.centerX*1.5, this.game.world.centerY, "Test_Building1");
        building2.anchor.set(0.5);
-       game.physics.arcade.enable(building2);
+       this.game.physics.arcade.enable(building2);
        building2.enableBody = true;
        building2.body.immovable = true;
 
@@ -133,14 +88,14 @@ GameContainer.stGame.prototype = {
 
    },//end_create
    update: function(){
-      player.rotation = game.physics.arcade.angleToPointer(player);
+      player.rotation = this.game.physics.arcade.angleToPointer(player);
 
-   if (game.input.activePointer.isDown){
+   if (this.game.input.activePointer.isDown){
 
       emitter.y = player.y + transformOverAngle(player.rotation, emitterSpriteOffsetX, emitterSpriteOffsetY).y;
       emitter.x = player.x + transformOverAngle(player.rotation, emitterSpriteOffsetX, emitterSpriteOffsetY).x;
 
-      distMouseCursorToEmitter = distanceBetween(game.input.mousePointer.x, game.input.mousePointer.y, emitter.x, emitter.y);
+      distMouseCursorToEmitter = distanceBetween(this.game.input.mousePointer.x, this.game.input.mousePointer.y, emitter.x, emitter.y);
 
       // handles the "spread" of water particles (the further you aim the narrower the stream)
       particleVelocityOffset = distMouseCursorToEmitter ;
@@ -155,11 +110,11 @@ GameContainer.stGame.prototype = {
 
       if(distMouseCursorToEmitter > waterStreamMaxDistance){
          var similarTriangleProportion = waterStreamMaxDistance/distMouseCursorToEmitter;
-         emitterToMouseDistanceX = similarTriangleProportion*(game.input.mousePointer.x-emitter.x);
-         emitterToMouseDistanceY = similarTriangleProportion*(game.input.mousePointer.y-emitter.y);
+         emitterToMouseDistanceX = similarTriangleProportion*(this.game.input.mousePointer.x-emitter.x);
+         emitterToMouseDistanceY = similarTriangleProportion*(this.game.input.mousePointer.y-emitter.y);
       }else{
-         emitterToMouseDistanceX = game.input.mousePointer.x-emitter.x;
-         emitterToMouseDistanceY = game.input.mousePointer.y-emitter.y;
+         emitterToMouseDistanceX = this.game.input.mousePointer.x-emitter.x;
+         emitterToMouseDistanceY = this.game.input.mousePointer.y-emitter.y;
       }
 
       emitter.maxParticleSpeed = new Phaser.Point(emitterToMouseDistanceX+particleVelocityOffset, emitterToMouseDistanceY+particleVelocityOffset);
@@ -169,11 +124,11 @@ GameContainer.stGame.prototype = {
 
    }
 
-   game.physics.arcade.collide(emitter, building, particleBuildingOnCollision);
-   game.physics.arcade.collide(player, building);
+   this.game.physics.arcade.collide(emitter, building, particleBuildingOnCollision);
+   this.game.physics.arcade.collide(player, building);
 
-   game.physics.arcade.collide(emitter, building2, particleBuildingOnCollision);
-   game.physics.arcade.collide(player, building2);
+   this.game.physics.arcade.collide(emitter, building2, particleBuildingOnCollision);
+   this.game.physics.arcade.collide(player, building2);
 
 
 
@@ -197,16 +152,6 @@ player.body.velocity.y = 0;
    },//end_update
    render: function() {
    // display fps
-      game.debug.text('FPS: ' + game.time.fps, 20, 580, 'yellow');
+      this.game.debug.text('FPS: ' + this.game.time.fps, 20, 580, 'yellow');
    }
 };//end_s1Game
-
-//sets width and height of game to avaliable width and height
-var game = new Phaser.Game(800, 600, Phaser.AUTO);
-
-//add the game states to the game object
-game.state.add("stBoot", GameContainer.stBoot);
-game.state.add("stPreloadAssets", GameContainer.stPreloadAssets);
-game.state.add("stGame", GameContainer.stGame);
-//start the first state
-game.state.start("stBoot");
