6ab09873f2c591b8947edafefad91cbd3d9cddcf
Wilbur Li <wiwili@ucsc.edu>
2017-05-10 21:19:59 -0700

    Organization: Split files into multiple states. this. statements were needed.

---
+++ js/prefabs/Utility.js
@@ -0,0 +1,121 @@
+//logging functionality
+function l(itemToLog){
+if(true){
+      if(typeof(itemToLog)==Object){
+         console.log(JSON.stringify(itemToLog));
+      }else{
+         console.log(itemToLog);
+      }
+   }
+}
+// distance between
+function distanceBetween(x1, y1, x2, y2){
+   return Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2));
+}
+
+function transformOverAngle(angle, x, y){
+   //angle given in radians
+   cosine = Math.cos(angle);
+   sine = Math.sin(angle);
+   xprime = (cosine*x)+(-1*(sine*y));
+   yprime = (sine*x)+(cosine*y);
+   l("xprime: " + xprime);
+   l("yprime: " + yprime);
+   var returnObj = {x: xprime, y: yprime};
+   return returnObj;
+}
+
+//timer functionality
+function UtilTime(phaserGameObject){
+   this.gameTime = game.time;
+   this.initTime = this.gameTime.totalElapsedSeconds();
+   this.lapList = [];
+}
+UtilTime.prototype.constructor = UtilTime;
+UtilTime.prototype.resetTime = function(){
+   this.initTime = this.gameTime.totalElapsedSeconds();
+   this.lapList = [];
+};
+UtilTime.prototype.seconds = function(){
+   return (this.gameTime.totalElapsedSeconds()-this.initTime);
+};
+UtilTime.prototype.milisecs = function(){
+   return 1000*(this.gameTime.totalElapsedSeconds()-this.initTime);
+};
+UtilTime.prototype.lap = function(){
+   this.lapList.push(this.gameTime.totalElapsedSeconds()-this.initTime);
+};
+UtilTime.prototype.getLapTimes = function(){
+   return this.lapList;
+};
+UtilTime.prototype.getLap = function(lapIndex){
+   return this.lapList[lapIndex];
+};
+UtilTime.prototype.clearLaps = function(lapIndex){
+    this.lapList = [];
+};
+
+//keyboard manager
+function KeyManager(phaserGameObject){
+   this.keyboard = phaserGameObject.input.keyboard;
+   this.keyList = [];
+}
+KeyManager.prototype.constructor = KeyManager;
+KeyManager.prototype.addKey = function(phaserKeyCode){
+   var key = {code: Phaser.KeyCode[phaserKeyCode], isDown: false, isPressed: false, isReleased: false, lastState:  1};
+   this.keyList.push(key);
+};
+KeyManager.prototype.removeKey = function(phaserKeyCode){
+   this.keyList.splice(this.keyList.indexOf(this.keyList.find(function(keyElement){
+      return (keyElement.code == Phaser.KeyCode[phaserKeyCode]);
+   }, this)), 1);
+};
+KeyManager.prototype.isDown = function(phaserKeyCode){
+   //l("KeyManager's isDown called");
+   key = this.keyList.find(function(keyElement){
+      return (keyElement.code == Phaser.KeyCode[phaserKeyCode]);
+   }, this);
+   return key.isDown;
+};
+KeyManager.prototype.isPressed = function(phaserKeyCode){
+   key = this.keyList.find(function(keyElement){
+      return (keyElement.code == Phaser.KeyCode[phaserKeyCode]);
+   }, this);
+   return key.isPressed;
+};
+KeyManager.prototype.isReleased = function(phaserKeyCode){
+   key = this.keyList.find(function(keyElement){
+      return (keyElement.code == Phaser.KeyCode[phaserKeyCode]);
+   }, this);
+   return key.isReleased;
+};
+KeyManager.prototype.update = function(){
+
+   var keyboard = this.keyboard;
+
+   // 1: key not pressed (default), 2: key just pressed, 3: key pressed down, 4: key just released
+   this.keyList.forEach(function(key){
+      key.isDown = keyboard.isDown(key.code);
+      if(key.isDown){
+         if(key.lastState == 1 || key.lastState == 4){
+            key.isDown = false;
+            key.isPressed = true;
+            key.isReleased = false;
+            key.lastState = 2;
+         }else if(key.lastState == 2){
+            key.isPressed = false;
+            key.lastState = 3;
+         }
+      }else{ //key is down
+         if(key.lastState == 3 || key.lastState == 2){
+            key.isDown = true;
+            key.isPressed = false;
+            key.isReleased = true;
+            key.lastState = 4;
+         }else if(key.lastState == 4){
+            key.isReleased = false;
+            key.lastState = 1;
+         }
+      }
+   });
+};
